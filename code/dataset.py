28092975d42a4cc42ab3237d819c5870d717260d
plantpalfynn <plantpalfynn@gmail.com>
2024-06-27 17:24:09 +0200

    change dataset stuff

--- code/dataset.py
+++ code/dataset.py
@@ -1,13 +1,19 @@
 
-import torch
-import pandas as pd
-import urllib
-import tarfile
-import torchaudio
+import yaml
 
 #get the data ready for the Models 
-tar_path = download_asset("dataset/IWSLT23.tst2023.en-de.tar.gz")
-tar_item = "IWSLT23.tst2023.en-de/benchmark/en-de/tst2023/wav/ted_13587.wav"
-with tarfile.open(tar_path, mode="r") as tarfile_:
-    fileobj = tarfile_.extractfile(tar_item)
-    waveform, sample_rate = torchaudio.load(fileobj)
+
+def getsegments(): 
+    with open("/home/plantpalfynn/uni/BA/BA-Thesis/code/dataset/IWSLT23.tst2023.en-de/benchmark/en-de/tst2023/IWSLT.TED.tst2023.en-de.yaml", 'r') as file:
+        data = yaml.load(file, Loader=yaml.FullLoader)
+        for l in data:
+
+            print(l, "\n", l.get("wav"))
+
+
+getsegments()
+#tar_path = download_asset("dataset/IWSLT23.tst2023.en-de.tar.gz")
+#tar_item = "IWSLT23.tst2023.en-de/benchmark/en-de/tst2023/wav/ted_13587.wav"
+#with tarfile.open(tar_path, mode="r") as tarfile_:
+#    fileobj = tarfile_.extractfile(tar_item)
+#    waveform, sample_rate = torchaudio.load(fileobj)
