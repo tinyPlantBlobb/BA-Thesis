173cc8f325f87eb3735f843afcd8fef09b2f1bf4
plantpalfynn <plantpalfynn@gmail.com>
2024-07-09 17:34:11 +0200

    add in start to dropout based things

--- code/asr_dropout.py
+++ code/asr_dropout.py
@@ -1,55 +1,36 @@
-from sqlite3 import DatabaseError
 import torch.utils
 import torch.utils.data
 from datasets import Dataset, Audio
 from transformers import (
     WhisperProcessor,
     WhisperForConditionalGeneration,
-    WhisperFeatureExtractor,
 )
 import os
-
-# try:
-# import tensorflow  # required in Colab to avoid protobuf compatibility issues
-# except ImportError:
-#    pass
-
 import yaml
 import torch
-
-# import whisper
 import torchaudio
-
 from tqdm import tqdm
 
 # BASE = "$HOME"
-BASE = "/home/plantpalfynn/uni/BA-Thesis/code"
-# BASE = "/home/plantpalfynn/uni/BA/BA-Thesis/code"
+# BASE = "/home/plantpalfynn/uni/BA-Thesis/code"
+BASE = "/home/plantpalfynn/uni/BA/BA-Thesis/code"
 # import ipywidgets as widgets
 # Whisper  from the huggingface whisper implementation
 processor = WhisperProcessor.from_pretrained("openai/whisper-medium.en")
 # feature_extractor = WhisperFeatureExtractor.from_pretrained("openai/whisper-small")
 asr_model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-medium.en")
+
+#dropout set to 0.1 based on the paper that uses the same dropout as during training
+asr_model_drop = WhisperForConditionalGeneration.from_pretrained("openai/whisper-medium.en", dropout=0.1)
+
 options = dict(language="German", beam_size=5, best_of=5, dropout=0.3)
 transcribe_options = dict(task="transcribe", **options)
 # translate_options = dict(task="translate", **options)
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 if DEVICE == "cuda":
     asr_model = asr_model.to(DEVICE)
-# tar_path = download_asset("dataset/IWSLT23.tst2023.en-de.tar.gz")
-# tar_item = "IWSLT23.tst2023.en-de/benchmark/en-de/tst2023/wav/ted_13587.wav"
-# with tarfile.open(tar_path, mode="r") as tarfile_:
-#     fileobj = tarfile_.extractfile(tar_item)
-#     waveform, sample_rate = torchaudio.load(fileobj)
+
 asr_model.generation_config.forced_decoder_ids = None
-
-
-# def transcriptionProbability(tensor, **options):
-#     result = {
-#         "translationProb": torch.nn.functional.softmax(tensor, dim=-1),
-#         "translationMean": torch.mean(tensor, dim=-1).mean(dim=0),
-#     }
-#     return result
 
 
 def getsegments():
@@ -118,69 +99,77 @@ dataset = Dataset.from_dict(segmentAudio()).cast_column("audiofile", Audio())
 # # Returns the last layer proabbliities of the model as a dict containing the decoded text and the segments and the language
 asr_model.eval()
 result = {
-    "sample": [],
+    #"sample": [],
     "audiofile": [],
     "timestamp": [],
-    "res": [],
+    "ref": [],
     "logits": [],
     "softmax": [],
-#    "outputptobability": [],
+    #    "outputptobability": [],
     "transcription": [],
 }
 # print(dataset[1]["audiofile"])
-for i in tqdm(range(10)):
-    sample = dataset[i]
-    # for sample in tdqm(dataset):
-    print(sample["transcript"])
-    audio = waveform = sample["audiofile"]["array"]
-    sample_rate = sample["audiofile"]["sampling_rate"]  # alternatively set to 16000
-    text = sample["transcript"]
+with torch.no_grad():
+    for i in tqdm(range(10)):
+        sample = dataset[i]
+        # for sample in tdqm(dataset):
+        #print(sample["transcript"])
+        audio = waveform = sample["audiofile"]["array"]
+        sample_rate = sample["audiofile"]["sampling_rate"]  # alternatively set to 16000
+        text = sample["transcript"]
 
-    ########################################
-    # Github whisper implementation things #
-    ########################################
-    # # this will generate the log-mel spectrogram of the given audio which will serve as an input to the whisper model
-    # #mel = whisper.log_mel_spectrogram(audio)
-    # Since the whisper is a multilingual ASR model, this will detect the spoken language.
-    # #_, probs = asr_model.detect_language(mel)
-    # #print("Detected language is " + " " + str({max(probs, key=probs.get)}))
-    # # decode the audio
-    # #options = whisper.DecodingOptions()
-    # #result_trans = whisper.decode(asr_model, mel, options)
-    # result["transcription"].append(
-    #     asr_model.transcribe(audio, **transcribe_options)["text"]
-    # )
-    # translation = asr_model.transcribe(audio, **translate_options)["text"]
+        #############################################
+        # Huggingface whisper implementation things #
+        #############################################
 
-    #############################################
-    # Huggingface whisper implementation things #
-    #############################################
+        # this will return the last layer probabilities of the model
+        input = processor(audio, sampling_rate=16000, return_tensors="pt")
+        input_features = input.input_features.to(DEVICE)
+        # logitsmaybe = asr_model.generate(input_features=input_features, output_scores=True)
+        # print(logitsmaybe)
+        res = asr_model.generate(input_features=input_features)
+        
+        logits = asr_model(
+            input_features, decoder_input_ids=res
+        ).logits  # gets the last layer probabilities of the model
+        trans = processor.batch_decode(res, skip_special_tokens=True)[0]
+        # # get the frist average log probability of the model for that aucio
+        result["audiofile"].append(sample["audiofile"])
+        result["timestamp"].append(sample["timestamp"])
+        result["logits"].append(logits)
+        result["softmax"].append(torch.nn.functional.softmax(logits, dim=-1))
+        # result["outputptobability"].append(result[0].avg_logprob)
+        result["ref"].append(text)
+        #result["sample"].append(sample)
+        result["transcription"].append(trans)
+        print(trans)
+        print(text)
+        torch.cuda.empty_cache()
 
-    # this will return the last layer probabilities of the model
-    input = processor(audio, sampling_rate=16000, return_tensors="pt")
-    input_features = input.input_features.to(DEVICE)
-    print(input_features, "\n result:")
-    # logitsmaybe = asr_model.generate(input_features=input_features, output_scores=True)
-    # print(logitsmaybe)
-    res = asr_model.generate(input_features=input_features)
-    print(res, "\n")
+        ########################################
+        # Github whisper implementation things #
+        ########################################
+        # # this will generate the log-mel spectrogram of the given audio which will serve as an input to the whisper model
+        # #mel = whisper.log_mel_spectrogram(audio)
+        # Since the whisper is a multilingual ASR model, this will detect the spoken language.
+        # #_, probs = asr_model.detect_language(mel)
+        # #print("Detected language is " + " " + str({max(probs, key=probs.get)}))
+        # # decode the audio
+        # #options = whisper.DecodingOptions()
+        # #result_trans = whisper.decode(asr_model, mel, options)
+        # result["transcription"].append(
+        #     asr_model.transcribe(audio, **transcribe_options)["text"]
+        # )
+        # translation = asr_model.transcribe(audio, **translate_options)["text"]
 
-    logits = asr_model(
-        input_features, decoder_input_ids=res
-    ).logits  # gets the last layer probabilities of the model
-    print(logits)
-    # # get the frist average log probability of the model for that aucio
-    result["audiofile"].append(sample["audiofile"])
-    result["timestamp"].append(sample["timestamp"])
-    result["logits"].append(logits)
-    result["softmax"].append(torch.nn.functional.softmax(logits, dim=-1))
-    # result["outputptobability"].append(result[0].avg_logprob)
-    # result["res"].append(res)
-    result["sample"].append(sample)
 
-print(result)
-with open("result.txt", "w") as file:
-    #TODO better output file format, maybe a yaml?
-    file.write(str(result))
-# #segmentAudio()
+        ####################
+        # dropout based shit#
+        #####################
+
+
+with open("result.yaml", "w") as file:
+    # TODO better output file format, maybe a yaml?
+    torch.save(result, "result.pt")
+# file.write(str(result))
 file.close()
--- code/dataset.py
+++ code/dataset.py
@@ -1,55 +1,68 @@
-
-from os import close
 import yaml
 import xml.etree.ElementTree as ET
 
 
-#get the data ready for the Models 
-#BASE = "$HOME"
-#BASE = "/home/plantpalfynn/uni/BA-Thesis/code"
+# get the data ready for the Models
+# BASE = "$HOME"
+# BASE = "/home/plantpalfynn/uni/BA-Thesis/code"
 BASE = "/home/plantpalfynn/uni/BA/BA-Thesis/code"
 
-with open(BASE +"/dataset/IWSLT23.tst2023.en-de/benchmark/en-de/tst2023/IWSLT.TED.tst2023.en-de.yaml", 'r') as file:
+with open(
+    BASE
+    + "/dataset/IWSLT23.tst2023.en-de/benchmark/en-de/tst2023/IWSLT.TED.tst2023.en-de.yaml",
+    "r",
+) as file:
     timestamps = yaml.load(file, Loader=yaml.FullLoader)
     file.close()
-with open(BASE +"/dataset/IWSLT23.tst2023.en-de/FILE_ORDER", 'r') as wavlist:
-    wavlist = wavlist.readlines()
-    allwavs = [l.strip() for l in wavlist]
-    wavlist.close()
+with open(BASE + "/dataset/IWSLT23.tst2023.en-de/FILE_ORDER", "r") as wavs:
+    wavlist = wavs.readlines()
+    allwavs = [wav.strip() for wav in wavlist]
+    wavs.close()
 
-timestamps = {wav+".wav": [elem for elem in timestamps if elem.get("wav") == wav+".wav"] for wav in allwavs}
-#XML PART 
-detree = ET.parse(BASE +'/dataset/IWSLT23.tst2023.en-de/benchmark/en-de/tst2023/IWSLT.TED.tst2023.en-de.de.xml')
-entree = ET.parse(BASE +'/dataset/IWSLT23.tst2023.en-de/benchmark/en-de/tst2023/IWSLT.TED.tst2023.en-de.en.xml')
+timestamps = {
+    wav + ".wav": [elem for elem in timestamps if elem.get("wav") == wav + ".wav"]
+    for wav in allwavs
+}
+# XML PART
+detree = ET.parse(
+    BASE
+    + "/dataset/IWSLT23.tst2023.en-de/benchmark/en-de/tst2023/IWSLT.TED.tst2023.en-de.de.xml"
+)
+entree = ET.parse(
+    BASE
+    + "/dataset/IWSLT23.tst2023.en-de/benchmark/en-de/tst2023/IWSLT.TED.tst2023.en-de.en.xml"
+)
 deroot = detree.getroot()
 enroot = entree.getroot()
 
 for child in enroot:
     for refset in child:
-        
-        id = refset.attrib['docid']
+        id = refset.attrib["docid"]
         print(refset.attrib, id)
-        for i,doc in enumerate(refset):
-
+        for i, doc in enumerate(refset):
             print(doc.attrib, i, "\n next thing  \n")
             if doc.attrib != {}:
-               print(id)
-               print(timestamps["ted_"+id+".wav"][i-1] , doc.attrib, doc.text)
+                print(id)
+                print(timestamps["ted_" + id + ".wav"][i - 1], doc.attrib, doc.text)
 
-               timestamps["ted_"+id+".wav"][i-1]['transcript']= doc.text
+                timestamps["ted_" + id + ".wav"][i - 1]["transcript"] = doc.text
 for child in deroot:
     for refset in child:
-        id = refset.attrib['docid']
+        id = refset.attrib["docid"]
         print(refset.attrib, id)
-        for i,doc in enumerate(refset):
+        for i, doc in enumerate(refset):
             print(doc.attrib, i)
             if doc.attrib != {}:
-               print(timestamps["ted_"+id+".wav"][i] , doc.attrib, doc.text)
+                print(timestamps["ted_" + id + ".wav"][i], doc.attrib, doc.text)
 
-               timestamps["ted_"+id+".wav"][i]['translation']= doc.text
+                timestamps["ted_" + id + ".wav"][i]["translation"] = doc.text
 
 # DUmping everything into the yaml file
 
-with open(BASE +"/dataset/IWSLT23.tst2023.en-de/benchmark/en-de/tst2023/IWSLT.TED.tst2023.en-de.matched.yaml", 'w') as file:
+with open(
+    BASE
+    + "/dataset/IWSLT23.tst2023.en-de/benchmark/en-de/tst2023/IWSLT.TED.tst2023.en-de.matched.yaml",
+    "w",
+) as file:
     yaml.dump(timestamps, file)
     file.close()
