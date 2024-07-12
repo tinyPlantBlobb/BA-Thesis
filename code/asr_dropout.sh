b0f5dd87afb55c69402cd4a190cb39dd85126859
plantpalfynn <plantpalfynn@gmail.com>
2024-07-12 11:49:47 +0200

    some shit

--- code/asr_dropout.sh
+++ code/asr_dropout.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-# very simple example on how to use local $TMPDIR
-#SBATCH -N 1
-#SBATCH -t 24:00:00
-
+# running a distributed job with PyTorch
+#SBATCH --job-name=whisper-eval
+#SBATCH --nodes=1
+#SBATCH --ntasks=4
+#SBATCH --gres=gpu:1
+#SBATCH --gpus-per-task=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=24:00:00
+#SBATCH --output=whispereval.txt
 
 # Extract compressed input dataset on local SSD
 tar -C $TMPDIR/ -xvzf $(ws_find data-ssd)/dataset.tgz
@@ -10,7 +15,7 @@ tar -C $TMPDIR/ -xvzf $(ws_find data-ssd)/dataset.tgz
 # The application reads data from dataset on $TMPDIR and writes results to $TMPDIR
 #myapp -input $TMPDIR/dataset/myinput.csv -outputdir $TMPDIR/results
 
-python3 asr_dropout.py --data_dir $TMPDIR/dataset --output_dir $TMPDIR/results
+srun torchrun --nnodes=1:4 --nproc-per-node=$NUM_TRAINERS --max-restarts=3 --rdzv-id=$JOB_ID --rdzv-backend=c10d --rdzv-endpoint=$HOST_NODE_ADDR asr_dropout.py 
 
 # Before job completes save results on a workspace
 rsync -av $TMPDIR/results $(ws_find data-ssd)/results-${SLURM_JOB_ID}/
---
+++ code/env_installation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+pip install pytorch torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip install transformers
+pip install datasets
+pip install librosa
