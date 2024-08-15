5d2ca12a20fd8dea627a53bf740d7b9395765010
plantpalfynn <plantpalfynn@gmail.com>
2024-08-15 11:50:41 +0200

    add deltalm command

--- code/regularpipe.sh
+++ code/regularpipe.sh
@@ -30,6 +30,10 @@ srun torchrun --nnodes 1 --nproc_per_node 1 asr_regular.py
 srun torchrun --nnodes 1 --nproc_per_node 1 seamless_regular.py
 #srun torchrun --nnodes 1 --nproc_per_node 1 mnt_part.py
 
+#srun fairseq-generate $(ws_find iswslt-dataset)/data-bin/ \
+#    --path $(ws_find iswslt-dataset)/checkpoints/checkpoint_best.pt \
+#    --batch-size 128 --beam 5 --remove-bpe | tee $TMPDIR/results/fulltranscriptions.csv
+
 # Before job completes save results on a workspace
 rsync -av $TMPDIR/results/fulltranscriptions.csv $(ws_find iswslt-dataset)/results-${SLURM_JOB_ID}/
 rsync -av $TMPDIR/results $(ws_find iswslt-dataset)/results-${SLURM_JOB_ID}/
