cdc8bd999acb054271aa827aac2a12700ee13cc8
plantpalfynn <plantpalfynn@gmail.com>
2024-08-16 11:50:37 +0200

    add comments and change files

--- code/finetuneDLM/data/finetune.sh
+++ code/finetuneDLM/data/finetune.sh
@@ -1,24 +1,20 @@
+bash examples/prepare_iwslt14.sh /tmp/iwslt14
 
+bash examples/binary_iwslt14.sh \
+     /tmp/iwslt14/iwslt14.tokenized.de-en \
+     /tmp/iwslt14/iwslt14.spm \
+     /path/to/checkpoint/spm.model
 
-PRETRAINED_MODEL=/home/plantpalfynn/uni/BA/BA-Thesis/code/finetuneDLM/model.pt
-python train.py $data_bin \
-    --save-dir $save_dir \
-    --arch deltalm_base \
-    --pretrained-deltalm-checkpoint $PRETRAINED_MODEL \
-    --share-all-embeddings \
-    --max-source-positions 512 --max-target-positions 512 \
-    --criterion label_smoothed_cross_entropy \
-    --label-smoothing 0.1 \
-    --optimizer adam --adam-betas '(0.9, 0.98)' \
-    --lr-scheduler inverse_sqrt \
-    --lr $lr \
-    --warmup-init-lr 1e-07 \
-    --stop-min-lr 1e-09 \
-    --warmup-updates 4000 \
-    --max-update 400000 \
-    --max-epoch 100 \
-    --max-tokens $batch_size \
-    --update-freq 1 \
-    --seed 1 \
-    --log-format simple \
-    --skip-invalid-size-inputs-valid-test
+bash examples/binary_iwslt14.sh \
+     /tmp/iwslt14/iwslt14.spm \
+     /tmp/iwslt14/iwslt14.bin \
+     /path/to/checkpoint/dict.txt
+
+bash examples/train_iwslt14.sh \
+     /tmp/iwslt14/iwslt14.bin \
+     /tmp/iwslt14/checkpoints \
+     /path/to/checkpoint/model.pt
+
+bash examples/evaluate_iwslt14.sh \
+     /tmp/iwslt14/iwslt14.bin \
+     /tmp/iwslt14/checkpoints
