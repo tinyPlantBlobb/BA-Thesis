e66dccca8035f1d7e08aa4c7cddda1546d96fcca
plantpalfynn <plantpalfynn@gmail.com>
2024-08-22 14:33:06 +0200

    fix error in evaluation and add copy to bahs

--- code/evaluations.py
+++ code/evaluations.py
@@ -11,17 +11,34 @@ with open(TMPDIR + "/results/seamlessfulltranscriptions.csv", "r", newline="") a
         dialect="excel",
         fieldnames=["row", "transcript", "reference", "translation", "qe"],
     )
-    trans = translation = reference = tpscore = []
+    trans = translation = reference = tpscore = softmaxent = stddiv = []
+
     for r in reader:
         trans.append(r["transcript"])
         translation.append(r["translation"])
         reference.append(r["reference"])
-        tpscore = r["qe"][0]
-    refscore = [cometscore(trans, translation, reference) for row in reader]
+        print(r["qe"])
+        qe = r["qe"]
+        if qe != "qe":
+            tpscore = r["qe"][0]
+            softmaxent = r["qe"][1]
+            stddiv = r["qe"][2]
+    refscore = cometscore(trans, translation, reference)
 
     print(len(trans), len(translation), len(reference))
-    result = pearsoncorr(tpscore, refscore)
-    print(result)
+    tpresult = pearsoncorr(tpscore, refscore)
+    softres = pearsoncorr(softmaxent, refscore)
+    stdres = pearsoncorr(stddiv, refscore)
+    with open(TMPDIR + "/results/scores.txt", "w") as resfile:
+        resfile.write("reference scores\n")
+        resfile.write(refscore)
+        resfile.write("\n\n\n pearsoncorrelation of the Translation Probability")
+        resfile.write(tpresult)
+        resfile.write("\nsoftmax correlation\n")
+        resfile.write(softres)
+        resfile.write("\n standart div\n")
+        resfile.write(stdres)
+        resfile.close()
     # for row in reader:
     #    score = cometscore(row["transcript"], row["translation"], row["reference"])
     #    print(score, row["qe"])
