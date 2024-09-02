b0fe51bb8628a5ef015ed66fee4a9898ae2e801b
plantpalfynn <plantpalfynn@gmail.com>
2024-09-02 12:53:53 +0200

    change cvs format a bit

--- code/evaluations.py
+++ code/evaluations.py
@@ -5,12 +5,18 @@ import os
 import csv
 
 TMPDIR = os.environ["TMPDIR"]
+with open(TMPDIR + "/results/fulltranscriptions.csv", "r", newline="") as transfile:
+    reader = csv.DictReader(
+        transfile,
+        dialect="excel",
+        fieldnames=["row", "transcript", "reference", "translation"],
+    )
 
 with open(TMPDIR + "/results/seamlessfulltranscriptions.csv", "r", newline="") as file:
     reader = csv.DictReader(
         file,
         dialect="excel",
-        fieldnames=["row", "transcript", "reference", "translation", "qe"],
+        fieldnames=["row", "reference transcript", "reference translation", "transcription", "translation", "transcript prob", "transcript mean","qe"],
     )
     trans = []
     translation = []
@@ -20,59 +26,63 @@ with open(TMPDIR + "/results/seamlessfulltranscriptions.csv", "r", newline="") a
     stddiv = []
 
     for r in reader:
-        trans.append(r["transcript"])
-        translation.append(r["translation"])
-        reference.append(r["reference"])
-        print(r["qe"])
-        qe = r["qe"]
-        if qe != "qe":
+        if r["qe"] != "qe":
+            trans.append(r["transcript"])
+            translation.append(r["translation"])
+            reference.append(r["reference"])
+            print(r["qe"][1:-1].split(", "))
+            qe = r["qe"][1:-1].split(", ")
+
             tpscore.append(qe[0])
             softmaxent.append(qe[1])
             stddiv.append(qe[2])
     refscores = cometscore(trans, translation, reference)
+
     refscore = refscores["scores"]
-    with open(TMPDIR + "/resultscore.csv", "w") as resscorefile:
-        reswriter = csv.writer(resscorefile, dialect="excel")
-        reswriter.writerow(
-            [
-                "row",
-                "transcript",
-                "reference",
-                "translation",
-                "qe tp",
-                "qe softent",
-                "qe stddiv",
-                "refscore",
-            ]
-        )
-        for i in range(len(translation)):
-            reswriter.writerow(
-                [
-                    i,
-                    trans[i],
-                    reference[i],
-                    translation[i],
-                    tpscore[i],
-                    softmaxent[i],
-                    stddiv[i],
-                    refscore[i],
-                ]
-            )
-        resscorefile.close()
-    print(type(refscore))
+    # with open(TMPDIR + "/resultscore.csv", "w") as resscorefile:
+    #    reswriter = csv.writer(resscorefile, dialect="excel")
+    #    reswriter.writerow(
+    #        [
+    #            "row",
+    #            "transcript",
+    #            "reference",
+    #            "translation",
+    #            "qe tp",
+    #            "qe softent",
+    #            "qe stddiv",
+    #            "refscore",
+    #        ]
+    #    )
+    #    for i in range(len(translation) - 1):
+    #        print(i)
+    #        reswriter.writerow(
+    #            [
+    #                i,
+    #                trans[i],
+    #                reference[i],
+    #                translation[i],
+    #                tpscore[i],
+    #                softmaxent[i],
+    #                stddiv[i],
+    #                refscore[i],
+    #            ]
+    #        )
+    #    resscorefile.close()
+    # print(tpscore)
+    # print(refscore)
     print(len(trans), len(translation), len(reference))
     tpresult = pearsoncorr(tpscore, refscore)
     softres = pearsoncorr(softmaxent, refscore)
     stdres = pearsoncorr(stddiv, refscore)
     with open(TMPDIR + "/results/scores.txt", "w") as resfile:
-        resfile.write("reference scores\n")
-        resfile.write(refscore)
-        resfile.write("\n\n\n pearsoncorrelation of the Translation Probability")
-        resfile.write(tpresult)
+        # resfile.write("reference scores\n")
+        # resfile.write(str(refscore))
+        resfile.write("\n\n\n pearsoncorr of the translation probability")
+        resfile.write(str(tpresult))
         resfile.write("\nsoftmax correlation\n")
-        resfile.write(softres)
+        resfile.write(str(softres))
         resfile.write("\n standart div\n")
-        resfile.write(stdres)
+        resfile.write(str(stdres))
         resfile.close()
     # for row in reader:
     #    score = cometscore(row["transcript"], row["translation"], row["reference"])
