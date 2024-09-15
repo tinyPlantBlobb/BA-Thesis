a8a00d596d622219bb02c5ff87128e4fb7a480fb
plantpalfynn <plantpalfynn@gmail.com>
2024-09-15 23:05:10 +0200

    setup for fixing errors with the dropout dataset

--- code/testing.py
+++ code/testing.py
@@ -1,6 +1,8 @@
+from os import walk
 from datasets.features import translation
 import torch
-from qeLogic import TranslationProbability, softmaxEntropy, sentStd, getQE
+from qeLogic import TranslationProbability, softmaxEntropy, sentStd, readCSV
+from datasets import Dataset, load_dataset
 
 
 class Result:
@@ -41,16 +43,22 @@ class Result:
         return  # "audiofile: "+str(self.audiofile)+ "\n" + "timestamp: "+str(self.timestamp)+ "\n" + "ref: "+str(self.ref)+ "\n"
 
 
-with open(
-    "/home/plantpalfynn/uni/BA/BA-Thesis/code/results/seamless_result0.pt", "rb"
-) as f:
-    print(f)
-    data = torch.load(
-        "/home/plantpalfynn/uni/BA/BA-Thesis/code/results/seamless_result0.pt",
-        map_location=torch.device("cuda"),
-    )
-
-    # Load data from file
-    # print(type(data[0]))
-    generationdata = data[0]
-    qeres = getQE(generationdata, translation=True, dropout=False)
+BASE = "/home/plantpalfynn/uni/BA/BA-Thesis/code/"
+# dataset1 = load_dataset(
+#    "csv", data_files=BASE + "results/dropoutfulltranscriptions.csv"
+# )
+dataset = Dataset.from_dict(readCSV(BASE + "results/dropoutfulltranscriptions.csv"))
+print(dataset)
+# with open(
+#    "/home/plantpalfynn/uni/BA/BA-Thesis/code/results/seamless_result0.pt", "rb"
+# ) as f:
+#    print(f)
+#    data = torch.load(
+#        "/home/plantpalfynn/uni/BA/BA-Thesis/code/results/seamless_result0.pt",
+#        map_location=torch.device("cuda"),
+#    )
+#
+#    # Load data from file
+#    # print(type(data[0]))
+#    generationdata = data[0]
+#    qeres = getQE(generationdata, translation=True, dropout=False)
