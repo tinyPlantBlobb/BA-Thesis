801b28524ba39b9b9adb6174705e5767c11767c8
plantpalfynn <plantpalfynn@gmail.com>
2024-10-01 19:14:48 +0200

    fix options

--- code/nmt.sh
+++ code/nmt.sh
@@ -16,7 +16,8 @@ fairseq-preprocess \
 
 echo "preprocessing done"
 # todo fix path for model checkpoint (ask danni)
-srun fairseq-generate $(ws_find iswslt-dataset)/data-bin/data/ \
-  --path /project/OML/dliu/iwslt2023/model/mt/deltalm-large.tune.bilingual.de.diversify.adapt.TEDonly.clean/checkpoint_avg_last5.pt \
+srun fairseq-generate $(ws_find iswslt-dataset) \
+  --path checkpoint_avg_last5.pt \
+  --model-overrides "{'pretrained_deltalm_checkpoint': '~/deltalm-large.pt'}" \
   --source-lang en --target-lang de \
   --batch-size 128 --beam 5 --remove-bpe --results-path $(ws_find iswslt-dataset)/results-${SLURM_JOB_ID} | tee $TMPDIR/results/dlmtranscriptions.csv
