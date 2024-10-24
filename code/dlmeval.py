c40712d2cff560c09f4bd45bb44cbd91f8f719ff
plantpalfynn <plantpalfynn@gmail.com>
2024-10-24 20:07:13 +0200

    add regex

--- code/dlmeval.py
+++ code/dlmeval.py
@@ -1,4 +1,8 @@
-import os
+import re
 
 with open("generate-test.txt", "r") as f:
     lines = f.readlines()
+
+    output = [i for i in lines if re.search(r"[TPHSD]-\d.*", i)]
+
+# regex: [TPHSD]-\d.*
