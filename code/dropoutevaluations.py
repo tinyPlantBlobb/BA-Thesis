80b07f80bdf158d0153c777f30ab5e46935c5106
plantpalfynn <plantpalfynn@gmail.com>
2024-12-20 23:01:07 +0100

    finish the setup

--- code/dropoutevaluations.py
+++ code/dropoutevaluations.py
@@ -1,5 +1,5 @@
 import re
-from code import qeLogic
+import qeLogic
 from datasets.features import translation
 from qeLogic import cometscore, pearsoncorr, worderror
 import os
@@ -20,7 +20,7 @@ transcriptindex = ["transcript probability " + str(i) for i in range(0, 30)]
 row.extend(["transcript probability " + str(i) for i in range(0, 30)])
 row.extend(["transcript " + str(i) for i in range(0, 30)])
 row.extend(["translation probability" + str(i) for i in range(0, 30)])
-translationindex = ["translation probability " + str(i) for i in range(0, 30)]
+translationindex = ["translation probability" + str(i) for i in range(0, 30)]
 row.extend(["translation " + str(i) for i in range(0, 30)])
 with open(TMPDIR + "/results/translation0.csv", "r", newline="") as file:
     reader = csv.DictReader(
@@ -41,7 +41,8 @@ with open(TMPDIR + "/results/translation0.csv", "r", newline="") as file:
     translationqe = []
     translationdpprob = []
     translationdpvarinace = []
-
+    translationEntropyvariance = []
+    translationstddivvariance = []
     tpscore = []
     softmaxent = []
     stddiv = []
@@ -49,7 +50,7 @@ with open(TMPDIR + "/results/translation0.csv", "r", newline="") as file:
     for r in reader:
         # print(r)
         if r["qe"] != "qe":
-            transcripts.append(r["transcription"])
+            transcripts.append(r["transcript 0"])
             translation.append(r["regulartranslation"])
             reference_translation.append(r["reference translation"])
             reference_trancsript.append(r["reference transcript"])
@@ -58,23 +59,28 @@ with open(TMPDIR + "/results/translation0.csv", "r", newline="") as file:
             # transcriptionprob.append(r["transcript prob"])
             transcriptionprob.append(
                 [
-                    [float(j.strip()) for j in r[i].strip("()").split(",")]
+                    [float(j.strip()) for j in r[i].strip("([])").split(",")]
                     for i in transcriptindex
                 ]
             )
 
-            print(r["transcript mean"], " probability  ", r["transcript prob"])
+            # print(r["transcript mean"], " probability  ", r["transcript prob"])
             transcriptmean.append([t[1] for t in transcriptionprob])
             transcrptprobabiltiy.append([i[0] for i in transcriptionprob])
             qes = [
-                list(map(float, r[i].strip("()").split(","))) for i in translationindex
+                list(map(float, r[i].strip("([])").split(",")))
+                for i in translationindex
             ]
 
             translationqe.append(qes)
             translationdpprob.append(math.fsum([i[0] for i in qes]))
             translationdpvarinace.append(qeLogic.variance([i[0] for i in qes]))
+            translationEntropy.append(math.fsum([i[1] for i in qes]))
+            translationEntropyvariance.append(qeLogic.variance([i[1] for i in qes]))
+            translationstddiv.append(math.fsum([i[2] for i in qes]))
+            translationstddivvariance.append(qeLogic.variance([i[2] for i in qes]))
 
-    refscores = cometscore(transcripts, translation, reference_translation)
+    refscores = cometscore(reference_translation, translation, reference_translation)
 
     refscore = refscores["scores"]
     with open(TMPDIR + "referencescores.txt", "w") as export:
@@ -92,6 +98,7 @@ with open(TMPDIR + "/results/translation0.csv", "r", newline="") as file:
     softres = pearsoncorr(softmaxent, refscore)
     stdres = pearsoncorr(stddiv, refscore)
     transcriptresult = pearsoncorr(transcriptionprob, wer)
+
     print(transcriptresult, tpresult, softres, stdres)
     with open(TMPDIR + "/results/scores.txt", "w") as resfile:
         # resfile.write("reference scores\n")
@@ -107,54 +114,7 @@ with open(TMPDIR + "/results/translation0.csv", "r", newline="") as file:
         resfile.write("\n trasncript mean\n")
         resfile.write(str(meanresult["pearsonr"]))
         resfile.close()
-    # for row in reader:
-    #    score = cometscore(row["transcript"], row["translation"], row["reference"])
-    #    print(score, row["qe"])
-    # from datasets.features import translation
-    # from qeLogic import pearsoncorr, worderror, cometscore
-    # import os
-    # import csv
-    #
-    # world_size = 4
-    ## world_size = torch.cuda.device_count()
-    # transcripts = []
-    #
-    # translation = []
-    # reference_translation = []
-    # reference_trancsript = []
-    # transcriptionprob = []
-    # transcriptmean = []
-    # tpscore = []
-    # softmaxent = []
-    # stddiv = []
-    # fullthing = []
-    # for i in range(world_size):
-    #    with open(
-    #            TMPDIR + "results/dropouttranslationfulldropped" + str(i) + ".csv",
-    #        "r",
-    #    ) as resfile:
-    #        reader = csv.DictReader(
-    #            resfile,
-    #            dialect="excel",
-    #            fieldnames=row,
-    #        )
-    #        for r in reader:
-    #            if r["row"] != "row":
-    #                # print(type(r))
-    #                fullthing.append(r)
-    #
-    #
-    # with open("seamlessdropout.csv", "w") as csvfile:
-    #    writer = csv.DictWriter(csvfile, fieldnames=row)
-    #    writer.writerows(sorted(fullthing, key=lambda x: int(x["row"])))
-    # reference_scores =
-    #
-    ##with open("referencescores.txt", "r") as ref:
-    ##    lines = ref.readlines()
-    ##    reference_scores = [float(i) for i in lines]
-    #
-    # with open(("seamlessdropout.csv"), "r") as csvfile:
-    #    reader = csv.DictReader(csvfile, fieldnames=row)
+
     #    for row in reader:
     #        # transcripts.append(r["transcription"])
     #        translation.extend([row[i] for i in transcriptindex])
