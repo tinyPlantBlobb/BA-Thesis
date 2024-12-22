cb94c38a8a0a89cb291673f8ea8bf191e3f620af
plantpalfynn <plantpalfynn@gmail.com>
2024-12-22 17:37:32 +0100

    add images and change plotting

--- code/plotting.py
+++ code/plotting.py
@@ -5,14 +5,27 @@ args = argparse.ArgumentParser()
 args.add_argument("--inp", type=str, default="inp.txt")
 args.add_argument("--ref", type=str, default="ref.txt")
 args.add_argument("--split", type=bool, default=False)
+args.add_argument("--outimg", type=str, default="out.png")
 ref = args.parse_args().ref
 input = args.parse_args().inp
 splitting = args.parse_args().split
+outpath = args.parse_args().outimg
 with open(ref, "r") as reffile:
-    refs = list(map(float, reffile.readlines()))
+    lines = reffile.readlines()
+    refs = list(
+        map(lambda x: (float(x.split("\t")[0]), float(x.split("\t")[1])), lines)
+    )
+    wers = [i[1] for i in refs]
+    comets = [i[0] for i in refs]
+
     x = range(len(refs))
-    print(len(x), len(refs))
-    # plt.scatter(x, refs, color="yellow", label="Reference")
+    # print(len(x), len(refs))
+    plt.scatter(x, wers, color="yellow", label="Reference")
+    plt.savefig("werref.png")
+    plt.clf()
+    plt.scatter(x, comets, color="blue", label="Comet")
+    plt.savefig("references.png")
+    plt.clf()
 if input != "ref.txt":
     with open(input, "r") as inpfile:
         if splitting:
@@ -32,12 +45,12 @@ if input != "ref.txt":
                 len(dropprop),
                 len(dropvar),
             )
-            x = range(len(genprop))
-            plt.scatter(x, genprop, color="red", label="Probability")
-            plt.scatter(x, entropy, color="green", label="Entropy")
-            plt.scatter(x, stddiv, color="purple", label="Standard Deviation")
-            plt.scatter(x, dropprop, color="orange", label="Drop Probability")
-            plt.scatter(x, dropvar, color="black", label="Drop Variance")
+            # x = range(len(genprop))
+            plt.scatter(comets, genprop, color="red", label="Probability")
+            plt.scatter(comets, entropy, color="green", label="Entropy")
+            plt.scatter(comets, stddiv, color="purple", label="Standard Deviation")
+            plt.scatter(comets, dropprop, color="orange", label="Drop Probability")
+            plt.scatter(comets, dropvar, color="black", label="Drop Variance")
             # plt.scatter(x, inps, color="gold", label="Reference")
         else:
             inps = list(map(float, inpfile.readlines()))
@@ -45,4 +58,4 @@ if input != "ref.txt":
 
 plt.xlabel("dataset lines")
 plt.ylabel("probability")
-plt.show()
+plt.savefig(outpath)
