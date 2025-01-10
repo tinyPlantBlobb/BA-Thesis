aff8ee7276a628dea7d1fffa8583947a1e18ee2c
plantpalfynn <plantpalfynn@gmail.com>
2025-01-10 21:32:40 +0100

    change paths, update env installation first part, remove unneeded files

--- code/env_installation.sh
+++ code/env_installation.sh
@@ -2,6 +2,28 @@
 python3 -m venv qe-whitebox
 source qe-whitebox/bin/activate
 pip install pytorch torchaudio --index-url https://download.pytorch.org/whl/cu118
-pip3 install transformers
-pip3 install datasets
-pip3 install librosa
+pip install transformers
+pip install datasets
+pip install evaluate
+pip install librosa
+pip install matplotlib
+pip install numpy
+pip install sentencepiece
+pip install tqdm
+pip install protobuf
+pip install unbabel-comet
+pip install jiwer
+pip install werpy
+
+mkdir deltalm
+cd deltalm/
+git clone -n --depth=1 --recursive https://github.com/microsoft/unilm.git
+cd unilm/
+git sparse-checkout set --no-cone deltalm
+git checkout
+git submodule update --init deltalm/fairseq
+cd deltalm/
+curl https://raw.githubusercontent.com/tinyPlantBlobb/fairseq/refs/heads/main/fairseq/sequence_generator.py -o fairseq/fairseq/sequence_generator.py
+curl https://raw.githubusercontent.com/tinyPlantBlobb/fairseq/refs/heads/main/fairseq_cli/generate.py -o fairseq/fairseq_cli/generate.py
+
+pip install fairseq/
