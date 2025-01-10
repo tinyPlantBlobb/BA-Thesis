f7bb3ba5eb425226df0f5890db6190d7545d2dc7
plantpalfynn <plantpalfynn@gmail.com>
2025-01-10 23:14:01 +0100

    update deltalm and dropoutlessnmt scripts

--- code/dropoutlessnmt.sh
+++ code/dropoutlessnmt.sh
@@ -6,28 +6,31 @@
 #SBATCH --gres=gpu:4
 #SBATCH --time=1:30:00
 #SBATCH --output=deltadponceeval.txt
-SPMMODEL=$(ws_find finals)/spm.model
+SPMMODEL=$1/spm.model
 source qe-whitebox/bin/activate
 #export SPMMODEL
-ws=$(ws_find finals) 
-srun python spm.py $ws/data-bin/test.de $TMPDIR/test.spm.de $SPMMODEL
-srun python spm.py $ws/data-bin/test.en $TMPDIR/test.spm.en $SPMMODEL
+ws=$2
+output=$3
+base=$PWD
+dlmmodel=$4
+python spm.py $ws/data-bin/test.de $ws/test.spm.de $SPMMODEL
+python spm.py $ws/data-bin/test.en $ws/test.spm.en $SPMMODEL
 
 fairseq-preprocess \
   --source-lang en --target-lang de \
   --bpe sentencepiece \
-  --testpref $TMPDIR/test.spm \
-  --destdir $TMPDIR \
+  --testpref $ws/test.spm \
+  --destdir $ws \
   --workers 20 \
   --srcdict $ws/dict.en.txt --tgtdict $ws/dict.de.txt
 
 echo "preprocessing done"
 
-srun python ~/deltalm/generate.py $TMPDIR/ \
-  --path ~/checkpoint_avg_last5.pt \
-  --arch deltalm_large --model-overrides "{'pretrained_deltalm_checkpoint': 'deltalm-large.pt'}" \
+python ~$base/deltalm/generate.py $ws/ \
+  --path $dlmmodel \
+  --arch deltalm_large --model-overrides "{'pretrained_deltalm_checkpoint': '$ws/deltalm-large.pt'}" \
   --source-lang en --target-lang de --batch-size 1 --beam 1 \
-  --remove-bpe=sentencepiece --unkpen 5 --results-path $(ws_find finals)/results-${SLURM_JOB_ID}
+  --remove-bpe=sentencepiece --unkpen 5 --results-path $output
 
-#rm $ws/data-bin/test.spm.*
+rm $ws/test.spm.*
 #rm $ws/data-bin/data/test*
