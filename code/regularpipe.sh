7f002f89b27f28ba94bfe4c261ce81e008436a02
plantpalfynn <plantpalfynn@gmail.com>
2025-01-11 03:46:45 +0100

    fix curl path

--- code/regularpipe.sh
+++ code/regularpipe.sh
@@ -13,7 +13,7 @@ mkdir -p $ws/data
 tar -C $ws/data -vxzf $ws/segments_IWSLT-23.en-de.tar.gz
 source qe-whitebox/bin/activate
 
-curl https://raw.githubusercontent.com/google/sentencepiece/refs/heads/master/python/src/sentencepiece/sentencepiece_model_pb2.py -o /pfs/data5/home/kit/stud/utqma/qe-whitebox/lib64/python3.9/site-packages/sentencepiece/sentencepiece_model_pb2.py
+curl https://raw.githubusercontent.com/google/sentencepiece/refs/heads/master/python/src/sentencepiece/sentencepiece_model_pb2.py -o qe-whitebox/lib64/python3.9/site-packages/sentencepiece/sentencepiece_model_pb2.py
 TMPDIR=$1 output=$3 torchrun --nnodes 1 --nproc_per_node 1 asr_regular.py
 TMPDIR=$1 output=$3 torchrun --nnodes 1 --nproc_per_node 1 seamless_regular.py
 python evaluations.py
