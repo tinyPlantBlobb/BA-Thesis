09ed54b43f5dab561c20423a813d0bf8de0f7b9b
plantpalfynn <plantpalfynn@gmail.com>
2025-01-11 01:51:14 +0100

    update nmt and first seamless absed ones

--- code/asr_dropout.sh
+++ code/asr_dropout.sh
@@ -6,36 +6,17 @@
 #SBATCH --gres=gpu:4
 #SBATCH --time=9:30:00
 #SBATCH --output=dropouteval.txt
-module load devel/cuda/11.8
-nodes=($(scontrol show hostnames $SLURM_JOB_NODELIST))
-nodes_array=($nodes)
-head_node=${nodes_array[0]}
-echo Head node: $head_node
-head_node_ip=$(srun --nodes=1 --time=1:00 --ntasks=1 -w "$head_node" hostname --ip-address)
-
-echo Node IP: $head_node_ip
-export LOGLEVEL=INFO
-mkdir -p $TMPDIR/data
-tar -C $TMPDIR/data -vxzf $(ws_find finals)/segments_IWSLT-23.en-de.tar.gz
+ws=$2
+tar -C $TMPDIR/data -vxzf $ws/segments_IWSLT-23.en-de.tar.gz
 source qe-whitebox/bin/activate
-
-pip install transformers 
-pip install datasets 
-pip install evaluate 
-pip install librosa 
-pip install sentencepiece
-#pip install protobuf --upgrade
-pip install unbabel-comet
-pip install jiwer 
-curl https://raw.githubusercontent.com/google/sentencepiece/refs/heads/master/python/src/sentencepiece/sentencepiece_model_pb2.py -o /pfs/data5/home/kit/stud/utqma/qe-whitebox/lib64/python3.9/site-packages/sentencepiece/sentencepiece_model_pb2.py 
-srun torchrun --nnodes 1 --nproc_per_node 1 asr_dropout.py
+output=$3
+TMPDIR=$1
+curl https://raw.githubusercontent.com/google/sentencepiece/refs/heads/master/python/src/sentencepiece/sentencepiece_model_pb2.py -o /pfs/data5/home/kit/stud/utqma/qe-whitebox/lib64/python3.9/site-packages/sentencepiece/sentencepiece_model_pb2.py
+torchrun --nnodes 1 --nproc_per_node 1 asr_dropout.py
 echo "\n dropout done \n"
-rsync -av $TMPDIR/results/ $(ws_find finals)/results-$SLURM_JOB_ID/
-srun torchrun --nnodes 1 --nproc_per_node 1 seamless.py
+rsrun torchrun --nnodes 1 --nproc_per_node 1 seamless.py
 #echo "seamless done"
-cd $TMPDIR
-#srun python ~/dropoutevaluations.py
-#rsync -av $TMPDIR/results/scores.txt $(ws_find finals)/results-${SLURM_JOB_ID}/
+#cd $TMPDIR
+python dropoutevaluations.py $output
 
-rsync -av $TMPDIR/results/dropoutfulltranscriptions.csv $(ws_find finals)/results-${SLURM_JOB_ID}/
-rsync -av $TMPDIR/results/ $(ws_find finals)/results-${SLURM_JOB_ID}/
+#rsync -av $TMPDIR/results/scores.txt $(ws_find finals)/results-${SLURM_JOB_ID}/
--- code/seamless.sh
+++ code/seamless.sh
@@ -7,31 +7,16 @@
 #SBATCH --time=13:30:00
 #SBATCH --output=seamlesendtoend.txt
 
-nodes=($(scontrol show hostnames $SLURM_JOB_NODELIST))
-nodes_array=($nodes)
-head_node=${nodes_array[0]}
-echo Head node: $head_node
-head_node_ip=$(srun --nodes=1 --time=1:00 --ntasks=1 -w "$head_node" hostname --ip-address)
-
-echo Node IP: $head_node_ip
-export LOGLEVEL=INFO
+base=PWD
 mkdir -p $TMPDIR/data
 tar -C $TMPDIR/data -vxzf $(ws_find finals)/segments_IWSLT-23.en-de.tar.gz
 source qe-whitebox/bin/activate
 
-#pip install transformers --upgrade
-#pip install datasets --upgrade
-#pip install evaluate --upgrade
-#pip install librosa --upgrade
-#pip install sentencepiece
-#pip install protobuf
 cd $TMPDIR
 mkdir results
-cd 
+cd $base
 #cp ~/dropoutfulltranscriptions.csv $TMPDIR/results/dropoutfulltranscriptions.csv
 srun torchrun --nnodes 1 --nproc_per_node 1 seamlessendtoend.py
-ls $TMPDIR
-ls $TMPDIR/results
 #rsync -av $TMPDIR/results/fulltranscriptions.csv $(ws_find iswslt-dataset)/results-${SLURM_JOB_ID}/
 
-rsync -av $TMPDIR/results $(ws_find iswslt-dataset)/results-${SLURM_JOB_ID}/
+#rsync -av $TMPDIR/results $(ws_find iswslt-dataset)/results-${SLURM_JOB_ID}/
